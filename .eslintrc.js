467ca6fefc19f4e2b4c6efb0c68c44beb28fd7b1
YurikaInoue <yrik.t.sing@gmail.com>
2021-02-14 12:33:50 +0900

    Week6の勉強

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
-//変更後
+// .eslintrc.js
 module.exports = {
   env: {
     browser: true,
     es6: true,
   },
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:vue/recommended"],
   plugins: ["eslint-plugin-html"],
   parserOptions: {
     ecmaVersion: 2018,
+  },
+  globals: {
+    Vue: true,
   },
 }
